9057d8eb9594c3ebbb280add0dda458181c28b47
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-04-15 18:14:29 +0530

    add vercel.json and add fe uri to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,57 +1,60 @@
 import dotenv from "dotenv";
-import express from "express"
-import http from "http"
+import express from "express";
+import http from "http";
 import { WebSocket } from "ws";
 import { db } from "./db";
 import { Canvas } from "./db/models/canvas.model";
-import cookiesParser from "cookie-parser"
-import cors from "cors"
-dotenv.config()
+import cookiesParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
 
-const app = express()
-app.use(cors({
-    origin: "http://localhost:5173",
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://cloud-canvas.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(cookiesParser())
-app.use(express.json())
+app.use(cookiesParser());
+app.use(express.json());
 
-const server = http.createServer(app)
-const webSocket = new WebSocket.Server({server})
+const server = http.createServer(app);
+const webSocket = new WebSocket.Server({ server });
 
 webSocket.on("connection", async (ws) => {
-    console.log("new connection...");
-    // on message
-    ws.on("message", async (ms) => {
-        try {
-            const message = JSON.parse(ms.toString())
-            console.log(message.canvasId);
+  console.log("new connection...");
+  // on message
+  ws.on("message", async (ms) => {
+    try {
+      const message = JSON.parse(ms.toString());
+      console.log(message.canvasId);
 
-            const update = await Canvas.findByIdAndUpdate(message.canvasId, {
-                canvasElements: message.data
-            })
-            console.log(update);
-            
-        } catch (error) {
-            console.log(error);
-        }
+      const update = await Canvas.findByIdAndUpdate(message.canvasId, {
+        canvasElements: message.data,
+      });
+      console.log(update);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  // on disconnection
+  ws.on("close", () => {
+    console.log("disconnected");
+  });
+  // on error
+  ws.on("error", (error) => {
+    console.log(error);
+  });
+  // send
+  ws.send(
+    JSON.stringify({
+      type: "Connection_ack",
+      message: "ws connection success",
     })
-    // on disconnection
-    ws.on("close", () => {
-        console.log("disconnected");
-    })
-    // on error
-    ws.on("error", (error) => {
-        console.log(error);
-    })
-    // send
-    ws.send(JSON.stringify({
-        type: "Connection_ack",
-        message: "ws connection success"
-    }))
-})
+  );
+});
 
 db()
   .then(() => {
@@ -63,8 +66,8 @@ db()
     console.log(error);
   });
 
-import canvasRouter from "./routes/canvas.route"
-app.use("/api/v1", canvasRouter)
+import canvasRouter from "./routes/canvas.route";
+app.use("/api/v1", canvasRouter);
 
-import userRouter from "./routes/user.route"
-app.use("/api/v1", userRouter)
+import userRouter from "./routes/user.route";
+app.use("/api/v1", userRouter);
