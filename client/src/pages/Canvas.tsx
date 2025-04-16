cd16aeabd549489488c3dd7b39d6109ec487c6d9
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-04-17 02:18:58 +0530

    add auth check in canvas too

--- client/src/pages/Canvas.tsx
+++ client/src/pages/Canvas.tsx
@@ -1,74 +1,89 @@
-import { Excalidraw} from "@excalidraw/excalidraw";
+import { Excalidraw } from "@excalidraw/excalidraw";
 import "@excalidraw/excalidraw/index.css";
 
 import { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
-import {isEqual} from "lodash"
-import { useParams } from "react-router";
+import { isEqual } from "lodash";
+import { useNavigate, useParams } from "react-router";
 import { BACKEND_URI } from "@/utils/config";
 import { Loader } from "lucide-react";
+import { useCloudCanvasUserStore } from "@/store/user_store";
+import Cookies from "js-cookie";
 
 const Canvas = () => {
-  const [canvasElements, setCanvasElements] = useState<string[] | any>()
-  const [loading, setLoading] =  useState(false)
-  const params = useParams()
+  const [canvasElements, setCanvasElements] = useState<string[] | any>();
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
 
-  
+  const cookie = Cookies.get("canvas_cloud_auth");
+  const navigate = useNavigate()
+
   useEffect(() => {
-      (async () => {
-        setLoading(true)
-        const sendReq = await fetch(`${BACKEND_URI}/api/v1/canvas/fetch?canvasId=${params.id}`, {
-            method: "PUT"
-        })
+    if (
+      useCloudCanvasUserStore.getState().isUserAuthenticated &&
+      typeof cookie === "string" &&
+      typeof useCloudCanvasUserStore.getState().userName === "string"
+    ) {
+      navigate(`/dashboard/${useCloudCanvasUserStore.getState().userName}`);
+    } else {
+      navigate("/signin");
+    }
 
-        const res = await sendReq.json()
-
-        if (res.success) {
-            setCanvasElements(res.canvasElements.canvasElements)
-            setLoading(false)
+    (async () => {
+      setLoading(true);
+      const sendReq = await fetch(
+        `${BACKEND_URI}/api/v1/canvas/fetch?canvasId=${params.id}`,
+        {
+          method: "PUT",
         }
-    })()
-  }, [params.id])
-  
+      );
+
+      const res = await sendReq.json();
+
+      if (res.success) {
+        setCanvasElements(res.canvasElements.canvasElements);
+        setLoading(false);
+      }
+    })();
+  }, [params.id]);
 
   // ws connection for render
   const { sendJsonMessage } = useWebSocket(`wss://cloud-canvas.onrender.com`, {
     onMessage: (e) => {
-        try {
-            const data = JSON.parse(e.data)
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-  })
+      try {
+        const data = JSON.parse(e.data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
 
   // for local
 
   // handle canvas change
   const handleChange = async (drawings: string[] | any) => {
     const drawingCopy = drawings.map((drawing: string[] | any) => ({
-        ...drawing
-    }))
+      ...drawing,
+    }));
 
     if (!isEqual(canvasElements, drawingCopy)) {
-        setCanvasElements(drawingCopy)
-        
-        sendJsonMessage({
-            type: "New drawings",
-            data: drawingCopy,
-            canvasId: params.id
-        })
-    }
+      setCanvasElements(drawingCopy);
 
-  }
+      sendJsonMessage({
+        type: "New drawings",
+        data: drawingCopy,
+        canvasId: params.id,
+      });
+    }
+  };
 
   if (loading) {
     return (
-        <div className="flex justify-center items-center min-h-[30vh]">
-            <Loader className="animate-spin"/>
-        </div>
-    )
+      <div className="flex justify-center items-center min-h-[30vh]">
+        <Loader className="animate-spin" />
+      </div>
+    );
   }
 
   return (
@@ -79,8 +94,7 @@ const Canvas = () => {
           appState: { theme: "dark" },
         }}
         onChange={handleChange}
-      >
-      </Excalidraw>
+      ></Excalidraw>
     </div>
   );
 };
