0744e1cf413ce0eb2a4439ba6a3d1a2423790648
Raju Sarkar <rajusarkaroo83@gmail.com>
2025-09-12 15:29:02 +0530

    add dist in gitignore

--- server/dist/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-router.post("/user/signin", user_controller_1.signin);
-router.post("/user/signup", user_controller_1.signup);
-exports.default = router;
